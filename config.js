8cec4fd1ca22540515647a51d43fdf966e83fc5c
CMNPN <116182493+CMNPN@users.noreply.github.com>
2022-10-20 10:27:41 +0700

    Add files via upload
    Gift-20.10

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Trang Phạm';
+const giftUrl = 'Trang.png';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Trang Phạm';
+const contentCard = 'Chúc Mừng Ngày Quốc Tế Phụ Nữ 20/10 💕💕';
+
+const Image = 'Trang.jpg';
+const giftImage = 'bo-hoa-10-removebg.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
